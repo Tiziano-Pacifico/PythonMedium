e51aa0175a9db655b3681a05159fa965002a98d3
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-12-29 11:03:07 +0100

    Add app contents

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
@@ -14,5 +15,24 @@ with col2:
         , non mi stressate troppo grazie e a presto
     """
     st.info(content)
+content2 = """
+Below you can find some of the apps I have built in python. Feel free to contact me
+"""
+st.markdown(f"<p style='font-size: 20px;'>{content2}</p>", unsafe_allow_html=True)
 
-st.markdown("<p style='font-size: 20px;'>Below you can find some of the apps I have built in python. Feel free to contact me</p>", unsafe_allow_html=True)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+df = pd.read_csv("Files/data.csv", sep=";")
+with col3:
+ for index, row in df[:10].iterrows():
+     st.header(row["title"])
+     st.write(row['description'])
+     st.image("Files/images/" + row['image'])
+     st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("Files/images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
